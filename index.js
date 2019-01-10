0660346cc83ad1f834b803d3b61872a4b7e755ac
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-01-10 14:37:52 -0200

    Configurando Express para atender uma requisição simples

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.listen(3000, () => console.log('Servidor Rodando na Porta 3000'));
+
+app.get('/teste', (req, res)=>{
+    console.log('Recebida requisição de teste');
+    res.send('.OK');
+});
