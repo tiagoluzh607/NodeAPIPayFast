3a5e8f12cc630a70078d441d3646af8e7aa462d2
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-01-10 14:58:56 -0200

    Separando Rotas em Controllers

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.listen(3000, () => console.log('Servidor Rodando na Porta 3000'));
 
 app.get('/teste', (req, res)=>{
     console.log('Recebida requisição de teste');
-    res.send('.OK');
+    res.send('.OK Nodemon');
 });
