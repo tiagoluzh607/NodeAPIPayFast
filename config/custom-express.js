b3b28d54e1de065734f9c3e40c3b5958150ab2aa
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-01-10 16:37:45 -0200

    Enviando Um JSON via Post para o Servidor

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 
 module.exports = ()=>{
     var app = express();
     
+    app.use(bodyParser.urlencoded({extend: true})); //configura para o node saber como tratar o body da requisição quando vir do navegador por form
+    app.use(bodyParser.json()); //configura para o node saber como tratar o body da requisição quando vir de um json
+
     consign()
         .include( 'controllers' )
         .into(app);
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -4,4 +4,10 @@ module.exports = (app) => {
         console.log('Recebida requisição de teste');
         res.send('.OK Nodemon');
     });
+
+    app.post('/pagamentos/pagamento', (req,res)=>{
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('OK.');
+    });
 }
