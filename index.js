c0c00361ea54388ca90bbadde83fccbf2be10773
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-01-10 15:11:33 -0200

    Importanto as Rotas do Controller com o consign

--- index.js
+++ index.js
@@ -2,6 +2,3 @@ var app = require('./config/custom-express')();
 
 app.listen(3000, () => console.log('Servidor Rodando na Porta 3000'));
 
-var controllers = require('./controllers/pagamentos');
-controllers(app);
-
